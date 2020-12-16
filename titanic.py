934d1a8b455f4d7e86df09fbd0bad634b8a16571
Sandrino Becirbegovic <s.becirbegovic@gmail.com>
2020-12-16 02:09:16 +0100

    Test for Script

--- titanic.py
+++ titanic.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import random as rnd
 
+# Test for script
 # Algorithm
 from sklearn.ensemble import RandomForestClassifier
 # RandomForestClassiefier seems to work the best
